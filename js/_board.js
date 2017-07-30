058897849ab66a415354e141b42ed9514c68c618
eratoiklio <eratoiklio@gmail.com>
2017-07-30 18:19:52 +0200

    switch to coordinates

--- js/_board.js
+++ js/_board.js
@@ -6,11 +6,20 @@ class Board {
     {
         $("#board").find("div").droppable({
             drop: function(event, ui) {
-                console.log(this.id);
+
                 // console.log(ui);
                 //      console.log(event);
-                  let tabShip =$(ui.draggable).data("boat");
-                  console.log(tabShip);
+                var thisPos = $(ui.draggable).offset();
+          var boardPos = $("#board").find("div").offset();
+          var x = thisPos.left - boardPos.left;
+          var y = thisPos.top - boardPos.top;
+          console.log("parent left" +boardPos.left);
+          console.log("parent top" +boardPos.top);
+          console.log("child left" +thisPos.left);
+          console.log("child top" +thisPos.top);
+          console.log("x: "+x +" y: "+ y);
+                // $(ui.draggable).data("boat");
+
             }
         });
     }
