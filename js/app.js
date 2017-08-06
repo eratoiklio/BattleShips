07605952c99cf39350b108a60e9fffe0830b040e
eratoiklio <eratoiklio@gmail.com>
2017-08-06 21:09:46 +0200

    optializated boats setting

--- js/app.js
+++ js/app.js
@@ -35,19 +35,19 @@ $(() => {
     });
     const aiBoats = [];
     let id = 1;
-    for (let i = 0; i < 4; i++) {
-        aiBoats.push(new Boat("aiBoat" + id, null, 1));
+    aiBoats.push(new Boat("aiBoat" + id, null, 4));
+    for (let i = 0; i < 2; i++) {
+        aiBoats.push(new Boat("aiBoat" + id, null, 3));
         id++;
     }
     for (let i = 0; i < 3; i++) {
         aiBoats.push(new Boat("aiBoat" + id, null, 2));
         id++;
     }
-    for (let i = 0; i < 2; i++) {
-        aiBoats.push(new Boat("aiBoat" + id, null, 3));
+    for (let i = 0; i < 4; i++) {
+        aiBoats.push(new Boat("aiBoat" + id, null, 1));
         id++;
     }
-    aiBoats.push(new Boat("aiBoat" + id, null, 4));
     const aiBoard = new AiBoard(aiBoats);
     aiBoard.setAllAiBoats();
     console.log(aiBoard.aiSetBoats);
